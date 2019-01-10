8fc5c56e9d0de8e2842c79db77c61ff43c182522
rishabhnk <rishabhnk@gmail.com>
2019-01-10 08:15:27 -0500

    updated

--- projects/predictBloodDonations/predictBloodDonations.py
+++ projects/predictBloodDonations/predictBloodDonations.py
@@ -14,12 +14,6 @@ y_train = trainingDataset.iloc[:, 5].values
 
 X_test = testingDataset.iloc[:, [1,2,3,4]].values
 
-"""
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
-"""
-
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
@@ -27,25 +21,26 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Fitting classifier to the Training set
-from sklearn.svm import SVC
-classifier = SVC(kernel = 'rbf', probability = 1, random_state = 0)
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
-y_pred = classifier.predict_proba(X_test)
+y_pred = classifier.predict_proba(X_train)
 y_pred = y_pred[:, 1]
+y_predLabel = classifier.predict(X_train)
 
-y_predLabel = classifier.predict(X_test)
-
-"""
-# Making the Confusion Matrix
+# Regression Metrics - Confusion Matrix, Accuracy score, CLassification Report
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_train, y_pred)
-"""
+cm = confusion_matrix(y_train, y_predLabel)
 
-# Regression Metrics - Mean absolute error
-
-""" ... """
+classifier.score(X_train, y_train)
+from sklearn.metrics import accuracy_score
+accuracy_score(y_train, y_predLabel)
+    """=0.9236"""
+    
+from sklearn.metrics import classification_report
+print(classification_report(y_train, y_predLabel))
 
 #Backward Elimination
 import statsmodels.formula.api as sm
@@ -53,3 +48,4 @@ X = np.append(arr = np.ones((576, 1)).astype(int), values = X_train, axis = 1)
 X_opt = X[:, [0, 1, 2, 3, 4]]
 regressor_OLS = sm.OLS(endog = y_train, exog = X_opt).fit()
 regressor_OLS.summary()
+
