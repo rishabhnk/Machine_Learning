5e72aeb03a922ddde62fad1ce6e7b459f01847cf
rishabhnk <rishabhnk@gmail.com>
2019-01-13 08:17:48 -0500

    update

--- projects/predictBloodDonations/predictBloodDonations.py
+++ projects/predictBloodDonations/predictBloodDonations.py
@@ -25,11 +25,12 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Fitting classifier to the Training set
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+from sklearn.svm import SVC 
+classifier = SVC(kernel = 'rbf', random_state = 0, probability = 1, gamma = 0.9, C = 3.25)
 classifier.fit(X_train, y_train)
 
-# Predicting the Test set results
+# Predicting the Test set results 
+#(results from the X data-set)
 y_pred = classifier.predict_proba(X_test)
 y_pred = y_pred[:, 1]
 y_predLabel = classifier.predict(X_test)
@@ -50,10 +51,21 @@ accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, c
 accuracies.mean()
 accuracies.std()
 
+# Applying Grid Search to find the best model and best parameters
+from sklearn.model_selection import GridSearchCV
+parameters= [ 
+                {'C': [1,2,3], 'kernel': ['linear']},
+                {'C': [ 3, 3.25, 3.3,], 'kernel': ['rbf'], 'gamma': [ 0.85, 0.9, 0.95]}
+            ]
+grid_search = GridSearchCV(estimator = classifier, param_grid= parameters, scoring = 'accuracy', cv = 10, n_jobs = -1)
+grid_search = grid_search.fit(X_train, y_train)
+
+best_acc = grid_search.best_score_
+best_param = grid_search.best_params_
+
 #Backward Elimination
 import statsmodels.formula.api as sm
 X = np.append(arr = np.ones((576, 1)).astype(int), values = X, axis = 1)
 X_opt = X[:, [0, 1, 2, 3, 4]]
 regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
-
